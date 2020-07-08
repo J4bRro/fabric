0b29dc233c27c288ebb42fe375b7c969b1be1e45
J4bRro <48897804+J4bRro@users.noreply.github.com>
2020-07-08 10:23:36 +0800

    Add files via upload

---
+++ sdkInit/fabricInitInfo.go
@@ -0,0 +1,20 @@
+package sdkInit
+
+import (
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
+)
+
+type InitInfo struct {
+	ChannelID      string
+	ChannelConfig  string
+	OrgAdmin       string
+	OrgName        string
+	OrdererOrgName string
+	OrgResMgmt     *resmgmt.Client
+
+	ChaincodeID      string
+	ChaincodeVersion string
+	ChaincodeGoPath  string
+	ChaincodePath    string
+	UserName         string
+}
---
+++ service/chaincodeFunc.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+)
+
+func (t *ServiceSetup) AddRecord(key string, value string) (string, error) {
+	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "addRecord", Args: [][]byte{[]byte(key), []byte(value)}}
+	respone, err := t.Client.Execute(req)
+	if err != nil {
+		return "", err
+	}
+	return string(respone.TransactionID), nil
+}
+
+func (t *ServiceSetup) DelRecord(key string) (string, error) {
+	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "delRecord", Args: [][]byte{[]byte(key)}}
+	respone, err := t.Client.Execute(req)
+	if err != nil {
+		return "", err
+	}
+	return string(respone.TransactionID), nil
+}
+
+func (t *ServiceSetup) UpdateRecord(key string, value string) (string, error) {
+	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "updateRecord", Args: [][]byte{[]byte(key), []byte(value)}}
+	respone, err := t.Client.Execute(req)
+	if err != nil {
+		return "", err
+	}
+	return string(respone.TransactionID), nil
+}
+
+func (t *ServiceSetup) SearchRecord(key string) (string, error) {
+	req := channel.Request{ChaincodeID: t.ChaincodeID, Fcn: "searchRecord", Args: [][]byte{[]byte(key)}}
+	respone, err := t.Client.Query(req)
+	if err != nil {
+		return "", err
+	}
+	return string(respone.Payload), nil
+}
