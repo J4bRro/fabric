5c95b4d8d66edc85cbe644b454453afc7daa15ba
J4bRro <J4bRro@protonmail.com>
2020-07-09 18:13:51 +0800

    fixed bug

--- sdkInit/start.go
+++ sdkInit/start.go
@@ -17,7 +17,7 @@ import (
 
 // const ChaincodeVersion = "1.0"
 
-func SetupSDK(ConfigFile string, initialized bool) (*fabsdk.FabricSDK, error) {
+func SetupSDK(ConfigFile string, initialized bool, info *InitInfo) (*fabsdk.FabricSDK, error) {
 
 	if initialized {
 		return nil, fmt.Errorf("Fabric SDK已被实例化")
@@ -29,21 +29,24 @@ func SetupSDK(ConfigFile string, initialized bool) (*fabsdk.FabricSDK, error) {
 	}
 
 	fmt.Println("Fabric SDK初始化成功")
-	return sdk, nil
-}
-
-func CreateChannel(sdk *fabsdk.FabricSDK, info *InitInfo) error {
 
 	clientContext := sdk.Context(fabsdk.WithUser(info.OrgAdmin), fabsdk.WithOrg(info.OrgName))
 	if clientContext == nil {
-		return fmt.Errorf("根据指定的组织名称与管理员创建资源管理客户端Context失败")
+		return nil, fmt.Errorf("根据指定的组织名称与管理员创建资源管理客户端Context失败")
 	}
 
 	// New returns a resource management client instance.
 	resMgmtClient, err := resmgmt.New(clientContext)
 	if err != nil {
-		return fmt.Errorf("根据指定的资源管理客户端Context创建通道管理客户端失败: %v", err)
+		return nil, fmt.Errorf("根据指定的资源管理客户端Context创建通道管理客户端失败: %v", err)
 	}
+
+	info.OrgResMgmt = resMgmtClient
+
+	return sdk, nil
+}
+
+func CreateAndJoinChannel(sdk *fabsdk.FabricSDK, info *InitInfo) error {
 
 	// New creates a new Client instance
 	mspClient, err := mspclient.New(sdk.Context(), mspclient.WithOrg(info.OrgName))
@@ -60,14 +63,12 @@ func CreateChannel(sdk *fabsdk.FabricSDK, info *InitInfo) error {
 	// SaveChannelRequest holds parameters for save channel request
 	channelReq := resmgmt.SaveChannelRequest{ChannelID: info.ChannelID, ChannelConfigPath: info.ChannelConfig, SigningIdentities: []msp.SigningIdentity{adminIdentity}}
 	// save channel response with transaction ID
-	_, err = resMgmtClient.SaveChannel(channelReq, resmgmt.WithRetry(retry.DefaultResMgmtOpts), resmgmt.WithOrdererEndpoint(info.OrdererOrgName))
+	_, err = info.OrgResMgmt.SaveChannel(channelReq, resmgmt.WithRetry(retry.DefaultResMgmtOpts), resmgmt.WithOrdererEndpoint(info.OrdererOrgName))
 	if err != nil {
 		return fmt.Errorf("创建应用通道失败: %v", err)
 	}
 
 	fmt.Println("通道已成功创建，")
-
-	info.OrgResMgmt = resMgmtClient
 
 	// allows for peers to join existing channel with optional custom options (specific peers, filtered peers). If peer(s) are not specified in options it will default to all peers that belong to client's MSP.
 	err = info.OrgResMgmt.JoinChannel(info.ChannelID, resmgmt.WithRetry(retry.DefaultResMgmtOpts), resmgmt.WithOrdererEndpoint(info.OrdererOrgName))
